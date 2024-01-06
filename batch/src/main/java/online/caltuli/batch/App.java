531ef7a5b4376dc93c8295aff0cdbedf07aff510
Caltuli <lucas.caltuli@gmail.com>
2024-01-06 20:06:56 +0100

    Initial commit

---
+++ batch/src/main/java/online/caltuli/batch/App.java
@@ -0,0 +1,13 @@
+package online.caltuli.batch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
